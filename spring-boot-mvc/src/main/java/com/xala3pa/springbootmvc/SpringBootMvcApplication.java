b599e1f0d2b3da9347f5312ff6689db3a5e21b77
xala3pa <salazar3pa@gmail.com>
2017-12-27 22:21:39 +0100

    Curated lists initial project structure

---
+++ spring-boot-mvc/src/main/java/com/xala3pa/springbootmvc/SpringBootMvcApplication.java
@@ -0,0 +1,12 @@
+package com.xala3pa.springbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcApplication.class, args);
+	}
+}
