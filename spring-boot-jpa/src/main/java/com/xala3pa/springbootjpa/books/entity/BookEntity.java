64dac3f8ea2d482ccf44738303513928afec3891
xala3pa <salazar3pa@gmail.com>
2018-01-08 23:35:19 +0100

    Constructor to entity added

--- spring-boot-jpa/src/main/java/com/xala3pa/springbootjpa/books/entity/BookEntity.java
+++ spring-boot-jpa/src/main/java/com/xala3pa/springbootjpa/books/entity/BookEntity.java
@@ -8,10 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class BookEntity {
